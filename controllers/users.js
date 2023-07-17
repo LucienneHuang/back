759152c24defc5b7cf2e071b04429399b2d17641
LucienneHuang <q22641949@gmail.com>
2023-07-17 16:03:49 +0800

    TEST

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,6 @@ import { StatusCodes } from 'http-status-codes'
 import users from '../models/users.js'
 import { getMessageFromValidationError } from '../utils/error.js'
 import jwt from 'jsonwebtoken'
-import products from '../models/products.js'
 
 export const create = async (req, res) => {
   try {
@@ -107,77 +106,5 @@ export const getProfile = (req, res) => {
       success: false,
       message: '發生錯誤'
     })
-  }
-}
-
-export const getCart = async (req, res) => {
-  try {
-    const result = await users.findById(req.user._id, 'cart').populate('cart.product')
-    res.status(StatusCodes.OK).json({
-      success: true,
-      message: '',
-      result: result.cart
-    })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: '發生錯誤'
-    })
-  }
-}
-
-export const editCart = async (req, res) => {
-  try {
-    // 尋找購物車內有沒有傳入的商品 ID
-    const idx = req.user.cart.findIndex(cart => cart.product.toString() === req.body.product)
-    if (idx > -1) {
-      // 如果購物車內已經有商品
-      // 檢查修改後的數量
-      const quantity = req.user.cart[idx].quantity + parseInt(req.body.quantity)
-      if (quantity <= 0) {
-        // 小於 0，移除
-        req.user.cart.splice(idx, 1)
-      } else {
-        // 大於 0，修改
-        req.user.cart[idx].quantity = quantity
-      }
-    } else {
-      // 如果購物車內沒有，檢查商品 ID 是否存在
-      const product = await products.findById(req.body.product)
-      if (!product || !product.sell) {
-        // 沒有，錯誤
-        throw new Error('NOT FOUND')
-      } else {
-        // 有，放入購物車
-        req.user.cart.push({
-          product: product._id,
-          quantity: req.body.quantity
-        })
-      }
-    }
-    // 保存
-    await req.user.save()
-    res.status(StatusCodes.OK).json({
-      success: true,
-      message: '',
-      result: req.user.cart.reduce((total, current) => total + current.quantity, 0)
-    })
-  } catch (error) {
-    if (error.message === 'NOT FOUND') {
-      res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-        message: '找不到'
-      })
-    } else if (error.name === 'ValidationError') {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        success: false,
-        message: getMessageFromValidationError(error)
-      })
-    } else {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        message: '發生錯誤'
-      })
-    }
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import contentType from '../middlewares/contentType.js'
-import { create, login, logout, extend, getProfile, getCart, editCart } from '../controllers/users.js'
+import { create, login, logout, extend, getProfile } from '../controllers/users.js'
 import * as auth from '../middlewares/auth.js'
 
 const router = express.Router()
@@ -10,7 +10,5 @@ router.post('/login', contentType('application/json'), auth.login, login)
 router.delete('/logout', auth.jwt, logout)
 router.patch('/extend', auth.jwt, extend)
 router.get('/me', auth.jwt, getProfile)
-router.get('/cart', auth.jwt, getCart)
-router.post('/cart', contentType('application/json'), auth.jwt, editCart)
 
 export default router
